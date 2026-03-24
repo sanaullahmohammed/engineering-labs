65fc6efaf4862ef46d0340abe2c4e83ffa0244d5
sanaullahmohammed <sanaullah.mohammad100@gmail.com>
2026-03-24 12:38:48 +0530

    feat: add square root

---
+++ sem-1-or-2-c-programming-for-problem-solving/Sem 1or2 - CPS/10_square_root.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main() {
+    float m, n, x;
+    n = 0.000001;
+    printf("Enter a number:\n");
+    scanf("%f",&x);
+    if(x < 1)
+        printf("Invalid number\n");
+    else
+    {
+        for(m=0; m<x; m+=n)
+        
+    }    
+}
