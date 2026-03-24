75c870faa33e6196a24b26c9c36dfd7a00ee497d
sanaullahmohammed <sanaullah.mohammad100@gmail.com>
2026-03-24 13:00:07 +0530

    feat: add udp client and server

---
+++ sem-5-computer-networks-and-security/part-a-java/UdpClient.java
@@ -0,0 +1,36 @@
+import java.net.*;
+
+public class UdpClient {
+    public void display(DatagramPacket packet)
+    {
+    byte[] p = packet.getData();
+
+    for (int i=0; i < packet.getLength(); i++)
+        System.out.print((char)p[i]);
+
+    System.out.println();
+    }
+
+    public void client() throws Exception
+    {
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket packet;
+
+        packet = new DatagramPacket(new byte[1024], 1024, InetAddress.getByName("localhost"), 3333);
+        socket.send(packet);
+
+        packet = new DatagramPacket(new byte[1024], 1024);
+        do
+        {
+            socket.receive(packet);
+            display(packet);
+        }while (Boolean.TRUE);
+        
+        socket.close();
+    }
+    public static void main(String args[]) throws Exception
+    {
+        UdpClient uc = new UdpClient();
+        uc.client();
+    }
+}
